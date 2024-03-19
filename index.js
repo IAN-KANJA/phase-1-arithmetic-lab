7d06f6d4fd803214d602ae8ad8a046778df709cb
iannjau <lantanacreations@gmail.com>
2024-03-19 20:09:08 +0300

    add index.js

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
-
-// Write your code here
+function multiply(){
+    const num1 =`number`
+    const num2 = `number`
+    console.log(multiply === 62)
+    return "62"
+}
+function random(){
+   return "generates a random integer greater than 0"   
+}
+function mod(){
+    const num3 =`number`
+    const num4 =`number`
+    console.log(mod === `4`)
+return   ` is an equation that calculates a remainder that is equal to 4` 
+}
+function max(){
+    console.log("will return 20 as the highest number in the set")
+    return max === `20`
+}
